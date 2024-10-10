318f9d91d1bf2d438824f6141271b3683589c5c1
Maxim Olifirenko <opaolifirenkomaxim@gmail.com>
2024-10-10 12:28:16 +0800

    Added lesson function in main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,25 @@
 import java.util.*
 
+fun String.isValidCCNumber(): Boolean {
+    var array: IntArray = intArrayOf()
+    var i: Int = 1
+    val inverse = this.reversed()
+
+    inverse.map {
+        if (it.isDigit()) {
+            val x = it.digitToInt()
+            val plus = (if (i%2 == 0) (if (x*2 > 9) (x*2 - 9) else x*2) else x)
+            array += plus
+            i++
+        }
+    }
+
+    return array.sum() % 10 == 0
+}
+
 fun main() {
-    val name = "Petya"
-    name.let { n -> println(n) }
+    val strings = arrayOf("foo", "bar", null, "", 123)
+    for (s in strings){
+        s?.let { print ("$it ") }
+    }
 }
