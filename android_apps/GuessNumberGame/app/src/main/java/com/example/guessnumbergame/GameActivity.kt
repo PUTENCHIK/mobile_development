c7f4d1bd6994b82b0164ab38651764a970c00935
PUTENCHIK <opaolifirenkomaxim@gmail.com>
2024-11-14 19:10:51 +0800

    Completed task GuessNumberGame

--- android_apps/GuessNumberGame/app/src/main/java/com/example/guessnumbergame/GameActivity.kt
+++ android_apps/GuessNumberGame/app/src/main/java/com/example/guessnumbergame/GameActivity.kt
@@ -1,8 +1,11 @@
 package com.example.guessnumbergame
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,15 +20,55 @@ class GameActivity : AppCompatActivity() {
         setContentView(R.layout.activity_game)
 
         left = intent.getIntExtra("left", 0)
-        right = intent.getIntExtra("right", 0)
+        right = intent.getIntExtra("right", 100)
 
-        Log.d("mytag", "($left, $right)")
+//        Log.d("set_limits", "($left, $right)")
+        updateQuestion()
+    }
+
+    fun getMid(): Int = (right + left) / 2
+
+    fun updateQuestion() {
+        if (left == right) {
+            return showAnswer(left)
+        }
+        val tv_question = findViewById<TextView>(R.id.question)
+        tv_question.text = when {
+            (right - left == 1) -> "($left, $right)\nЧисло равно ${left}?"
+            else -> "($left, $right)\nЧисло больше ${getMid()}?"
+        }
+    }
+
+    fun showAnswer(answer: Int) {
+        val tv_question = findViewById<TextView>(R.id.question)
+        tv_question.text = "Это $answer"
+        val ll_buttons = findViewById<LinearLayout>(R.id.answerContainer)
+        ll_buttons.visibility = View.INVISIBLE
     }
 
     fun onYesNoClick(view: View) {
         when (view.id) {
-            R.id.yes -> left = 0
-            R.id.no -> right = 0
+            R.id.yes -> {
+                if (right - left == 1) {
+                    showAnswer(left)
+                } else {
+                    left = getMid()+1
+                    updateQuestion()
+                }
+            }
+            R.id.no -> {
+                if (right - left == 1) {
+                    showAnswer(right)
+                } else {
+                    right = getMid()
+                    updateQuestion()
+                }
+            }
         }
+    }
+
+    fun backToMain(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
--- android_apps/GuessNumberGame/app/src/main/java/com/example/guessnumbergame/MainActivity.kt
+++ android_apps/GuessNumberGame/app/src/main/java/com/example/guessnumbergame/MainActivity.kt
@@ -18,21 +18,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    fun showError(text: String) {
+        val tv_error = findViewById<TextView>(R.id.error)
+        tv_error.visibility = View.VISIBLE
+        tv_error.text = text
+    }
+
     fun onGuessClick(view: View) {
         val intent = Intent(this, GameActivity::class.java)
         val et_left = findViewById<EditText>(R.id.left)
         val et_right = findViewById<EditText>(R.id.right)
-        val tv_error = findViewById<TextView>(R.id.error)
+
         val value: Int
 
         if (et_left.text.length > 0) {
             value = et_left.text.toString().toInt()
             intent.putExtra("left", value)
         } else {
-            // Make error block visible
-//            tv_error.visibility
+            return showError(getString(R.string.error_no_left))
         }
-        intent.putExtra("right", 999)
+
+        if (et_right.text.length > 0) {
+            if (value >= et_right.text.toString().toInt()) {
+                return showError(getString(R.string.error_bad_values))
+            }
+            intent.putExtra("right", et_right.text.toString().toInt())
+        } else {
+            return showError(getString(R.string.error_no_right))
+        }
+
         startActivity(intent)
     }
 }
