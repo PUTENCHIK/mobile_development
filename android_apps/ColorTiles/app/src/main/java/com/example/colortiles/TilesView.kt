0e8768341cc03b13e2f0984c5b6d1be5128815b6
PUTENCHIK <opaolifirenkomaxim@gmail.com>
2024-12-02 17:34:41 +0800

    Completed task ColorTiles

--- android_apps/ColorTiles/app/src/main/java/com/example/colortiles/TilesView.kt
+++ android_apps/ColorTiles/app/src/main/java/com/example/colortiles/TilesView.kt
@@ -6,32 +6,40 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.view.MotionEvent
 import android.view.View
-import androidx.core.view.setPadding
+import android.widget.Toast
 import kotlin.random.Random
-import kotlin.random.nextUInt
+
 
 class TilesView(context: Context): View(context) {
-    // onDraw
-    // onTouch
     private var h: Float = 1000f
     private var w: Float = 1000f
     private val n: Int = 4
+    private val margin: Int = 50
     private val padding: Int = 3
     private val paint = Paint()
     private val gridPaint = Paint()
+
+    private val matrix: Array<BooleanArray> = Array(n){ BooleanArray(n){ false } }
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
         h = (bottom - top).toFloat()
         w = (right - left).toFloat()
         gridPaint.color = Color.rgb(40, 40, 40)
+
+        for (i in 0..n-1) {
+            for (j in 0..n-1) {
+                matrix[i][j] = getRandomState()
+            }
+        }
     }
 
-    fun getRandomColor(): Int {
-        return if (Random.nextInt(0, 2) == 0)
-                    Color.BLACK
-                else
-                    Color.WHITE
+    fun getRandomState(): Boolean {
+        return Random.nextBoolean()
+    }
+
+    fun getRandomColor(state: Boolean): Int {
+        return if (state) Color.WHITE else Color.BLACK
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -40,19 +48,56 @@ class TilesView(context: Context): View(context) {
 
         for (i in 0..n-1) {
             for (j in 0..n-1) {
-                paint.color = getRandomColor()
+                paint.color = getRandomColor(matrix[i][j])
                 canvas.drawRect(
-                    j * w/n + if (j == 0) padding else padding,
-                    i * w/n + if (i == 0) padding else padding,
-                    (j+1) * w/n - if (j == n-1) padding else padding,
-                    (i+1) * w/n - if (i == n-1) padding else padding,
+                    j * w/n + padding,
+                    i * w/n + padding,
+                    (j+1) * w/n - padding,
+                    (i+1) * w/n - padding,
                     paint)
             }
         }
     }
 
+    fun invertRow(index: Int, except: Int) {
+        for (j in 0..n-1) {
+            if (j != except) {
+                matrix[index][j] = !matrix[index][j]
+            }
+        }
+    }
+
+    fun invertColumn(index: Int, except: Int) {
+        for (i in 0..n-1) {
+            if (i != except) {
+                matrix[i][index] = !matrix[i][index]
+            }
+        }
+    }
+
+    fun checkGameOver() {
+        val value: Boolean = matrix[0][0]
+        for (i in 0..n-1) {
+            for (j in 0..n-1) {
+                if (matrix[i][j] != value)
+                    return
+            }
+        }
+        val toast: Toast = Toast.makeText(context, "Game Over!", Toast.LENGTH_LONG)
+        toast.show()
+    }
+
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        event?.x
+        val i: Int = (event?.y!! / w * n).toInt()
+        val j: Int = (event.x / w * n).toInt()
+
+        if (i < n && j < n) {
+            matrix[i][j] = !matrix[i][j]
+            invertRow(i, j)
+            invertColumn(j, i)
+            invalidate()
+        }
+        checkGameOver()
 
         return super.onTouchEvent(event)
     }
---
+++ android_apps/ColorTiles/readme.md
@@ -0,0 +1,15 @@
+# Головоломка ColorTiles
+
+## Демонстрация работы
+
+<table>
+    <tbody>
+        <tr>
+            <td><img src="images/1.jpg" height="506" width="240"></td>
+            <td><img src="images/2.jpg" height="506" width="240"></td>
+        </tr>
+        <tr>
+            <td colspan=2 align="center"><img src="images/3.jpg" height="506" width="240"></td>
+        </tr>
+    </tbody>
+</table>
