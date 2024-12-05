e8560fdce83594b1f2227854ebba27001028ef1e
PUTENCHIK <opaolifirenkomaxim@gmail.com>
2024-12-05 16:17:05 +0800

    Completed game Memorina

--- android_apps/MemorinaGame/app/src/main/java/com/example/memorinagame/MainActivity.kt
+++ android_apps/MemorinaGame/app/src/main/java/com/example/memorinagame/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.example.memorinagame
 
+import android.app.ActionBar.LayoutParams
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.setMargins
 import androidx.core.view.setPadding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlin.reflect.typeOf
 
 class MainActivity : AppCompatActivity() {
     val images = mapOf(
@@ -24,21 +27,38 @@ class MainActivity : AppCompatActivity() {
         "pin" to R.drawable.pin,
         "sovunya" to R.drawable.sovunya,
     )
-    var correctAnswers: Int = 0
+    val back = R.drawable.back
     val n: Int = 4
+
+    var correctAnswers: Int = 0
+    var tries: Int = 0
     var openedCard: ImageView? = null
     var openedCardsAmount: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val layout = LinearLayout(applicationContext)
+        val layout: LinearLayout = LinearLayout(applicationContext)
+        layout.setId(R.id.mainLayout)
         layout.orientation = LinearLayout.VERTICAL
         layout.setPadding(30)
+
+        val header = LinearLayout(applicationContext)
+        header.orientation = LinearLayout.VERTICAL
+        val headerParams = LinearLayout.LayoutParams(
+            LayoutParams.MATCH_PARENT,
+            LayoutParams.WRAP_CONTENT
+        )
+        headerParams.setMargins(0, 0, 0, 20)
+        header.layoutParams = headerParams
 
         val tvCorrectAnswers = TextView(applicationContext)
         tvCorrectAnswers.setId(R.id.tvCorrectAnswers)
         tvCorrectAnswers.textSize = 22f
+
+        val tvTries = TextView(applicationContext)
+        tvTries.setId(R.id.tvTries)
+        tvTries.textSize = 22f
 
         val field = LinearLayout(applicationContext)
         field.orientation = LinearLayout.VERTICAL
@@ -50,7 +70,7 @@ class MainActivity : AppCompatActivity() {
             for (i in 1..2) {
                 imageViews.add(
                     ImageView(applicationContext).apply {
-                        setImageResource(R.drawable.ic_launcher_background)
+                        setImageResource(back)
                         layoutParams = params
                         tag = character.key
                         setOnClickListener(cardListener)
@@ -58,6 +78,7 @@ class MainActivity : AppCompatActivity() {
                 )
             }
         }
+        imageViews.shuffle()
 
         val rows = Array(4, { LinearLayout(applicationContext) })
         var count = 0
@@ -70,68 +91,98 @@ class MainActivity : AppCompatActivity() {
             field.addView(row)
         }
 
-        layout.addView(tvCorrectAnswers)
+        val btnReset = Button(applicationContext)
+        btnReset.apply {
+            text = "Заново"
+            textSize = 22f
+            width = LinearLayout.LayoutParams.MATCH_PARENT
+            height = LinearLayout.LayoutParams.WRAP_CONTENT
+            setOnClickListener(btnResetListener)
+        }
+
+        header.addView(tvCorrectAnswers)
+        header.addView(tvTries)
+        layout.addView(header)
         layout.addView(field)
+        layout.addView(btnReset)
         setContentView(layout)
     }
 
-    fun updateCorrectAnswers() {
+    fun updateHeader() {
         var tvCorrectAnswers = findViewById<TextView>(R.id.tvCorrectAnswers)
-        tvCorrectAnswers.text = "Correct answers: " + correctAnswers.toString()
+        tvCorrectAnswers.text = "Пар найдено: " + correctAnswers.toString()
+
+        var tvTries = findViewById<TextView>(R.id.tvTries)
+        tvTries.text = "Попыток: " + tries.toString()
     }
 
     suspend fun checkAnswers(card: View) {
-        changeCardStatus(card, true)
         when (openedCardsAmount) {
             0 -> {
                 if (card is ImageView) {
-                    openedCard = card
                     openedCardsAmount++
+                    changeCardStatus(card, true)
+                    openedCard = card
                 }
             }
             1 -> {
                 openedCardsAmount++
-                delay(1500)
+                tries++
+                changeCardStatus(card, true)
                 if (card.tag == openedCard?.tag) {
                     correctAnswers++
+                    updateHeader()
+                    delay(1500)
                     hideCard(card)
                     hideCard(openedCard!!)
+                    checkGameOver()
                 } else {
+                    updateHeader()
+                    delay(1500)
                     changeCardStatus(card, false)
                     changeCardStatus(openedCard!!, false)
                 }
                 openedCardsAmount -= 2
                 openedCard = null
             }
-            else -> {
-                var a: Int
-            }
         }
 
     }
 
-    suspend fun changeCardStatus(card: View, open: Boolean) {
+    fun changeCardStatus(card: View, open: Boolean) {
         if (card is ImageView) {
             card.setImageResource(when (open) {
                     true -> images.get(card.tag)!!
-                    false -> R.drawable.ic_launcher_background
+                    false -> back
                 }
             )
             card.isClickable = !open
         }
     }
 
-    suspend fun hideCard(card: View) {
+    fun hideCard(card: View) {
         if (card is ImageView) {
             card.visibility = View.INVISIBLE
             card.isClickable = false
         }
     }
 
+    fun checkGameOver() {
+        if (correctAnswers == 8) {
+            val toast: Toast = Toast.makeText(applicationContext,
+                "Вы нашли все пары!",
+                Toast.LENGTH_LONG
+            )
+            toast.show()
+        }
+    }
+
     val cardListener = View.OnClickListener() {
         GlobalScope.launch(Dispatchers.Main)
         { checkAnswers(it) }
+    }
 
-        updateCorrectAnswers()
+    val btnResetListener = View.OnClickListener() {
+        recreate()
     }
 }
---
+++ android_apps/MemorinaGame/readme.md
@@ -0,0 +1,16 @@
+# Игра "Меморина"
+
+## Часть 1. Динамическая вёрстка
+
+## Часть 2. Логика игры
+
+<table>
+    <tr>
+        <td><img src="images/2_1.jpg" width=360></td>
+        <td><img src="images/2_2.jpg" width=360></td>
+    </tr>
+    <tr>
+        <td><img src="images/2_3.jpg" width=360></td>
+        <td><img src="images/2_4.jpg" width=360></td>
+    </tr>
+</table>
