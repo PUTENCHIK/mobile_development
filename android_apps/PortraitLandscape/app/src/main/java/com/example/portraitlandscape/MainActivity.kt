22b9484d639b0f256cd9d34f107d0bd8a3d1d0b2
PUTENCHIK <opaolifirenkomaxim@gmail.com>
2024-12-12 19:12:11 +0800

    Completed task PortraitLandscape

--- android_apps/PortraitLandscape/app/src/main/java/com/example/portraitlandscape/MainActivity.kt
+++ android_apps/PortraitLandscape/app/src/main/java/com/example/portraitlandscape/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.portraitlandscape
 
+import android.app.Activity
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     private val images = listOf(R.drawable.pudge, R.drawable.baratrum, R.drawable.sf)
@@ -15,11 +15,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        index = getSharedPref().getInt("index", 0)
+        updateImage()
+    }
+
+    fun getSharedPref(): SharedPreferences {
+        return this.getSharedPreferences(
+            "portrait_landscape",
+            Activity.MODE_PRIVATE
+        )
     }
 
     fun updateImage() {
         val ivImage = findViewById<ImageView>(R.id.image)
         ivImage.setImageResource(images[index])
+        val tvCounter = findViewById<TextView>(R.id.counter)
+        tvCounter.text = resources.getString(R.string.counter_text, index+1, images.size)
+
+        with (getSharedPref().edit()) {
+            putInt("index", index)
+            apply()
+        }
     }
 
     fun previousImage(view: View) {
---
+++ android_apps/PortraitLandscape/readme.md
@@ -0,0 +1,8 @@
+# Адаптация разметки для альбомной/портретной ориентации
+
+## Демонстрация
+
+<table>
+    <tr><td><img src="images/2.jpg"></td></tr>
+    <tr><td><img src="images/1.jpg" width=360></td></tr>
+</table>
