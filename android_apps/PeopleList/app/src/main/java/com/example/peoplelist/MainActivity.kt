2c5ea5c85bd92fee4870d0792a382540372fb624
PUTENCHIK <opaolifirenkomaxim@gmail.com>
2024-12-19 17:15:00 +0800

    Completed task PeopleList

--- android_apps/PeopleList/app/src/main/java/com/example/peoplelist/MainActivity.kt
+++ android_apps/PeopleList/app/src/main/java/com/example/peoplelist/MainActivity.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,22 +13,45 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     private var people: MutableList<String> = mutableListOf()
+    lateinit var adapter: ArrayAdapter<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val lv_people = findViewById<ListView>(R.id.people)
-//        val adapter = ArrayAdapter<String>(this, R.layout.item, people)
-//        lv_people.adapter = adapter
+        adapter = ArrayAdapter<String>(this, R.layout.item, people)
+        lv_people.adapter = adapter
+
+        generateRandomData(30)
+    }
+
+    fun generateRandomData(n: Int) {
+        val names = resources.getStringArray(R.array.names)
+        val surnames = resources.getStringArray(R.array.surnames)
+        for (i in 1..n) {
+            addPerson("%s %s".format(names.random(), surnames.random()))
+        }
+    }
+
+    fun addPerson(name: String) {
+        people.add("%d. %s".format(people.size+1, name))
     }
 
     fun addPersonToList(view: View) {
         val et_person = findViewById<EditText>(R.id.et_person)
         val name = et_person.text.toString()
 
-        people.add(name)
-        et_person.setText("")
-        et_person.clearFocus()
+        if (name.length > 0) {
+            addPerson(name)
+            et_person.setText("")
+            et_person.clearFocus()
+            adapter.notifyDataSetChanged()
+        } else {
+            val toast: Toast = Toast.makeText(this,
+                resources.getString(R.string.error_message),
+                Toast.LENGTH_LONG)
+            toast.show()
+        }
     }
 }
