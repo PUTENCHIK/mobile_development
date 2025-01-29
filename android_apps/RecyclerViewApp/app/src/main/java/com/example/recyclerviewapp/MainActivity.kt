91bf5fe37862b9e5891101927aa3035f49a25330
PUTENCHIK <opaolifirenkomaxim@gmail.com>
2025-01-29 17:20:21 +0800

    Completed task 'Starting work with RecyclerView'

--- android_apps/RecyclerViewApp/app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
+++ android_apps/RecyclerViewApp/app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.recyclerviewapp
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity : AppCompatActivity() {
     val colors = generateColors()
@@ -21,15 +23,37 @@ class MainActivity : AppCompatActivity() {
         rv.adapter = adapter
     }
 
+    fun encodeColor(R: Int, G: Int, B: Int): Int {
+        return Color.rgb(R, G, B)
+    }
+
+    fun decodeColor(color: Int): List<Int> {
+        val code = listOf(
+            (color shr 16) and 0xff,
+            (color shr 8) and 0xff,
+            (color) and 0xff,
+        )
+        return code
+    }
+
     fun generateColors(): MutableList<Int> {
-        val n = 5
+        val n = 2
         val lower = Color.RED
         val upper = Color.BLUE
         val array = mutableListOf<Int>()
 
-        for (i in 1..n) {
-            array.add(lower + i/n * (upper - lower))
+        val lower_dec = decodeColor(lower)
+        val upper_dec = decodeColor(upper)
+
+        for (i in 0..n) {
+            val t = i.toDouble() / n
+            array.add(encodeColor(
+                (lower_dec[0] * (1 - t) + upper_dec[0] * t).toInt(),
+                (lower_dec[1] * (1 - t) + upper_dec[1] * t).toInt(),
+                (lower_dec[2] * (1 - t) + upper_dec[2] * t).toInt(),
+            ))
         }
+
         return array
     }
 }
---
+++ android_apps/RecyclerViewApp/readme.md
@@ -0,0 +1,11 @@
+# Начало работы с RecyclerView
+
+## Демонстрация
+
+<table>
+    <tr>
+        <td><img src="images/1.jpg" width=360></td>
+        <td><img src="images/2.jpg" width=360></td>
+        <td><img src="images/3.jpg" width=360></td>
+    </tr>
+</table>
